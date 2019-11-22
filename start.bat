d8a14cf2be93c2c158246fca7597dd69573888ef
Kyle Weishaar <k.weishak@gmail.com>
2019-11-22 14:54:00 -0500

    add general batch to run any quality check

---
+++ start.bat
@@ -0,0 +1,28 @@
+@ECHO off
+cls
+:start
+ECHO.
+ECHO 1. Qlik Sense
+ECHO 2. QlikView
+ECHO 3. NPrinting
+set choice=
+set /p choice=Which quality checks do you want to run? Select a number then press Enter.
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='1' "Sense_checks.bat"
+if '%choice%'=='2' "QV_checks.bat"
+if '%choice%'=='3' "NP_checks.bat"
+ECHO "%choice%" is not valid, try again
+pause
+ECHO.
+goto start
+:hello
+ECHO Qlik Sense
+goto end
+:bye
+ECHO QlikView
+goto end
+:test
+ECHO NPrinting
+goto end
+:end
+pause
